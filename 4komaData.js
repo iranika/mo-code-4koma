9a3d5b3b574efad4042285ebb0b56522ce4fb1a5
iranika <7370486+iranika@users.noreply.github.com>
2020-04-25 18:03:03 +0000

    update: auto updated 4komaData by workflow

--- 4komaData.js
+++ 4komaData.js
@@ -235,5 +235,5 @@ pageData = [
 ,{"Title": "温泉旅行", "ImagesUrl": ["https://mo4koma.iranika.info/4koma/ja/234-1.jpg", "https://mo4koma.iranika.info/4koma/ja/234-2.jpg", "https://mo4koma.iranika.info/4koma/ja/sp.jpg"]}
 ,{"Title": "春仕様", "ImagesUrl": ["https://mo4koma.iranika.info/4koma/ja/235-1.jpg", "https://mo4koma.iranika.info/4koma/ja/235-2.jpg", "https://mo4koma.iranika.info/4koma/ja/235-3.jpg", "https://mo4koma.iranika.info/4koma/ja/sp.jpg"]}
 ,{"Title": "いのしし", "ImagesUrl": ["https://mo4koma.iranika.info/4koma/ja/236-1.jpg", "https://mo4koma.iranika.info/4koma/ja/236-2.jpg", "https://mo4koma.iranika.info/4koma/ja/236-3.jpg", "https://mo4koma.iranika.info/4koma/ja/sp.jpg"]}
-,{"Title": "こうたい", "ImagesUrl": ["https://mo4koma.iranika.info/4koma/ja/237-1.jpg", "https://mo4koma.iranika.info/4koma/ja/sp.jpg"]}
+,{"Title": "こうたい", "ImagesUrl": ["https://mo4koma.iranika.info/4koma/ja/237-1.jpg", "https://mo4koma.iranika.info/4koma/ja/237-2.jpg", "https://mo4koma.iranika.info/4koma/ja/sp.jpg"]}
 ]
