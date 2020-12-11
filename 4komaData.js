d932377cf0dc33a57dc5dae3e23f05d183f4b2c4
iranika <7370486+iranika@users.noreply.github.com>
2020-12-11 06:24:26 +0000

    update: auto updated 4komaData by workflow

--- 4komaData.js
+++ 4komaData.js
@@ -250,5 +250,5 @@ pageData = [
 ,{"Title": "なっちゃん", "ImagesUrl": ["https://mo4koma.iranika.info/4koma/ja/249-1.jpg", "https://mo4koma.iranika.info/4koma/ja/sp.jpg"]}
 ,{"Title": "むかしばなし", "ImagesUrl": ["https://mo4koma.iranika.info/4koma/ja/250-1.jpg", "https://mo4koma.iranika.info/4koma/ja/250-2.jpg", "https://mo4koma.iranika.info/4koma/ja/250-3.jpg", "https://mo4koma.iranika.info/4koma/ja/sp.jpg"]}
 ,{"Title": "亥の日亥の月", "ImagesUrl": ["https://mo4koma.iranika.info/4koma/ja/251-1.jpg", "https://mo4koma.iranika.info/4koma/ja/251-2.jpg", "https://mo4koma.iranika.info/4koma/ja/251-3.jpg", "https://mo4koma.iranika.info/4koma/ja/sp.jpg"]}
-,{"Title": "写真", "ImagesUrl": ["https://mo4koma.iranika.info/4koma/ja/252-1.jpg", "https://mo4koma.iranika.info/4koma/ja/sp.jpg"]}
+,{"Title": "写真", "ImagesUrl": ["https://mo4koma.iranika.info/4koma/ja/252-1.jpg", "https://mo4koma.iranika.info/4koma/ja/252-2.jpg", "https://mo4koma.iranika.info/4koma/ja/sp.jpg"]}
 ]
