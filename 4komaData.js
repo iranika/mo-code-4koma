4060427b42197a7e5f98763c302b5f18051462aa
iranika <7370486+iranika@users.noreply.github.com>
2021-01-26 17:42:01 +0000

    update: auto updated 4komaData by workflow

--- 4komaData.js
+++ 4komaData.js
@@ -254,5 +254,5 @@ pageData = [
 ,{"Title": "ボーナス", "ImagesUrl": ["https://mo4koma.iranika.info/4koma/ja/253-1.jpg", "https://mo4koma.iranika.info/4koma/ja/253-2.jpg", "https://mo4koma.iranika.info/4koma/ja/sp.jpg"]}
 ,{"Title": "だまし絵", "ImagesUrl": ["https://mo4koma.iranika.info/4koma/ja/254-1.jpg", "https://mo4koma.iranika.info/4koma/ja/sp.jpg"]}
 ,{"Title": "牛歩", "ImagesUrl": ["https://mo4koma.iranika.info/4koma/ja/255-1.jpg", "https://mo4koma.iranika.info/4koma/ja/255-2.jpg", "https://mo4koma.iranika.info/4koma/ja/255-3.jpg", "https://mo4koma.iranika.info/4koma/ja/sp.jpg"]}
-,{"Title": "おともだち", "ImagesUrl": ["https://mo4koma.iranika.info/4koma/ja/256-1.jpg", "https://mo4koma.iranika.info/4koma/ja/sp.jpg"]}
+,{"Title": "おともだち", "ImagesUrl": ["https://mo4koma.iranika.info/4koma/ja/256-1.jpg", "https://mo4koma.iranika.info/4koma/ja/256-2.jpg", "https://mo4koma.iranika.info/4koma/ja/sp.jpg"]}
 ]
