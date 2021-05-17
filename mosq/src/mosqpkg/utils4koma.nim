a9535cf56a1b21122f5e3e16cb3c3bfd52fbd403
iranika <7370486+iranika@users.noreply.github.com>
2021-05-17 22:06:58 +0900

    refactor

--- mosq/src/mosqpkg/utils4koma.nim
+++ mosq/src/mosqpkg/utils4koma.nim
@@ -7,11 +7,12 @@ import os, times
 import uri
 import nimquery
 import json, marshal
-import strformat
+import strformat, sequtils
 import times
 import nre,options,strutils
 import threadpool
 {.experimental: "parallel".}
+
 
 # use get4komaUrl
 type 
@@ -19,8 +20,9 @@ type
     Title: string
     ImagesUrl: seq[string]
 
-const file4komaData = "4komaData.js.org"
+const file4komaDataOrg = "4komaData.js.org"
 const exportFile4komaData = "4komaData.js"
+const exportFile4komaDataJson = "4komaData.json"
 const originalSite = "http://momoirocode.web.fc2.com"
 const provideSite = "https://mo4koma.iranika.info/4koma/ja"
 #const sqAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36"
@@ -45,7 +47,8 @@ proc writeJson4komaData*[T](stream: T, url: string) =
     debugEcho $a.attr("href")
     res = client.get($(domain / a.attr("href")))
     let nodes = res.body.newStringStream().parseHtml().querySelectorAll("img")
-
+    
+    
     for img in nodes:
       if (".jpg" in img.attr("src")) or (".png" in img.attr("src")):
         let image_url = $(domain / "4koma" / img.attr("src").replace("./", ""))
@@ -59,18 +62,21 @@ proc writeJson4komaData*[T](stream: T, url: string) =
   stream.write "]"
 
 proc update4komaData*() =
-  var fp: File = open(file4komaData, FileMode.fmWrite)
+  var fp: File = open(file4komaDataOrg, FileMode.fmWrite)
   defer:
     close(fp)
   writeJson4komaData(fp, url=originalSite)
-  
+  #4komaData.jsと4komaData.jsonの生成
+  writeFile(exportFile4komaData, readFile(file4komaDataOrg).replace(originalSite & "/4koma", provideSite))
+  writeFile(exportFile4komaDataJson, readFile(exportFile4komaData).replace("pageData = ", ""))
+
 proc download4komaImage*() =
   if not existsDir("4koma"):
     createDir("4koma")
     createDir("4koma/ja")
   if not existsDir("4koma/ja"):
     createDir("4koma/ja")
-  let jsonNode4koma = readFile(file4komaData).replace("pageData = ", "").parseJson
+  let jsonNode4koma = readFile(file4komaDataOrg).replace("pageData = ", "").parseJson
   let list4koma: seq[PageData] = to(jsonNode4koma, seq[PageData])
   for index,item in list4koma:
     parallel:
@@ -83,29 +89,31 @@ proc download4komaImage*() =
         spawn hc.downloadFile(imgUrl, saveFilename)
         debugEcho "saved file: " & saveFilename
 
+
 proc updateFeedAtom*() =
-  let list4koma: seq[PageData] = readFile(file4komaData).replace("pageData = ", "").parseJson.to(seq[PageData])
-  let title = now().format("yyyyMMddHHmm")
+  let list4koma: seq[PageData] = readFile(file4komaDataOrg).replace("pageData = ", "").parseJson.to(seq[PageData])
+  let title = list4koma[^1].Title
+  let lastImage = list4koma[^1].ImagesUrl.filter(
+    proc (img: string): bool = img.contains("sp.jpg") == false
+  )[^1]
   let update = now().format("yyyy-MM-dd'T'HH':'mm':'sszzz")
-  let content = list4koma[list4koma.len - 1].Title
+  let published = now().format("yyyyMMddHHmm")
+  let content = """<img src="$lastImage" /> """ % ["lastImage", lastImage]
   let auther = "iranika"
-  let entry_url = "https://iranika.github.io/mo-code/#latest"
+  let entry_url = "https://movue.iranika.info/#/?page=latest"
 
   let addFeedEntryStr = """<!--insertEntry-->
   <entry>
     <id>tag:iranika.github.io,2019:Repository/194400309/$title</id>
     <updated>$update</updated>
+    <published>$published</published>
     <link rel="alternate" type="text/html" href="$entry_url"/>
     <title>$title</title>
     <content type="html">$content</content>
     <author>
       <name>$auther</name>
     </author>
-  </entry>""" % ["title", title, "update", update, "content", content, "auther", auther, "entry_url", entry_url]
+  </entry>""" % ["title", title, "update", update, "content", content, "auther", auther, "entry_url", entry_url, "published", published]
   writeFile("feed.atom", readFile("feed.atom").replace("<!--insertEntry-->", addFeedEntryStr))
   debugEcho addFeedEntryStr
-  return
-
-proc replaceUrl4komaJs*() =
-  writeFile(exportFile4komaData, readFile(file4komaData).replace(originalSite & "/4koma", provideSite))
   return
