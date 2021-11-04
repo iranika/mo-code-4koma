18322b927374b8f43e2061bce878fc554d0b6065
iranika <7370486+iranika@users.noreply.github.com>
2021-11-05 02:05:53 +0900

    add: top絵の更新feedを追加

---
+++ top/update-feed.ps1
@@ -0,0 +1,20 @@
+param (
+    $feedfile =".\feed.atom"
+)
+
+$dateTimeOffset = [System.DateTimeOffset]::Now
+$id = "tag:iranika.github.io,2019:Repository/194400309/" + $dateTimeOffset.ToString("yyyyMMddHHmmdd")
+$updated = $dateTimeOffset.ToString("yyyy-MM-ddTHH:mm:ssK")
+$entry = @"
+<!--insertEntry-->
+  <entry>
+    <id>$($id)</id>
+    <updated>$($updated)</updated>
+    <published>$($updated)</published>
+    <link rel="alternate" type="text/html" href="http://momoirocode.web.fc2.com/"/>
+    <title>TOP絵が更新されたよ</title>
+    <content type="html" xml:lang="ja">https://mo4koma.iranika.info/top/top.jpg</content>
+  </entry>
+"@
+
+(Get-Content $feedfile) -replace "<!--insertEntry-->", $entry | Out-File -Encoding utf8 $feedfile
