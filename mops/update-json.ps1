b9a2c945331962d6aeb42d3479f030c629f1bc5f
iranika <7370486+iranika@users.noreply.github.com>
2022-02-27 03:18:55 +0900

    add: output json file.

---
+++ mops/update-json.ps1
@@ -0,0 +1,47 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]
+    $SiteUrl = "http://momoirocode.web.fc2.com/mocode.html"
+    ,$OutFile = "4komaData.json"
+)
+
+$dom = Invoke-WebRequest $SiteUrl | Select-HtmlContent "li", @{
+    title = "a"
+    href = "a", ([AngleParse.Attr]::Href)
+}
+
+$list = $dom | % {
+    #NOTE: 相対参照から絶対参照に変更、
+    if (([string]$_.href).Contains("http://momoirocode.web.fc2.com")){
+        return $_
+    }else{
+        $result = $_
+        $result.href = $_.href -replace "^/", "http://momoirocode.web.fc2.com/"
+        return $result
+    }
+}
+
+class PageData {
+    [string] $title
+    [string] $baseUrl
+    [string[]] $images
+    PageData([string]$title, [string]$baseUrl, [string[]]$images){
+        $this.title = $title
+        $this.baseUrl = $baseUrl
+        $this.images = $images
+    }
+}
+
+$obj = $list | % {
+    $imgs = Invoke-WebRequest $_.href | Select-HtmlContent "img", @{
+        src = [AngleParse.Attr]::Src
+    } | ? { !([string]$_.src).Contains("counter_img.php?id=50") } | % { $_.src }
+    
+    $result = New-Object PageData($_.title, $_.href, $imgs)
+    Write-Information $result
+    return $result
+}
+
+$obj
+$obj | ConvertTo-Json | Out-File $OutFile -Encoding utf8
