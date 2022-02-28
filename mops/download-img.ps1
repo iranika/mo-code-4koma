0a43c8c5eed07e896030b26432e48c8c33086d34
iranika <7370486+iranika@users.noreply.github.com>
2022-02-28 23:01:03 +0900

    fix: loading AngelParse. and save path changed.

--- mops/download-img.ps1
+++ mops/download-img.ps1
@@ -6,9 +6,11 @@ param (
     ,[switch]$OnlyRecently #直近のみDLするオプション
 )
 
+Import-Module AngleParse
+
 $json = Get-Content $InputFile | ConvertFrom-Json
 $saveDir = "./4koma/"
-if (!(Test-Path $saveDir)) { mkdir $saveDir -Force }
+if (!(Test-Path "$saveDir/ja")) { mkdir "$saveDir/ja" -Force }
 
 $spfile = Join-Path $saveDir "sp.jpg"
 if (!(Test-Path $spfile)){
@@ -21,13 +23,14 @@ if (!$OnlyRecently) {
         #$VerbosePreference = $using:VerbosePreference
         $_.ImagesUrl | % {
             $DebugPreference = $using:DebugPreference
-            $savePath = Join-Path $using:saveDir $_
+            $savePath = Join-Path $using:saveDir "ja/$_"
+            $downloadFilePath = Join-Path $using:saveDir $_
             Write-Debug "savePath: $savePath"
             if (Test-Path $savePath){
                 Write-Debug "already file exists: $savePath"
             }else{
                 Write-Debug "new file"
-                $link = New-Object System.Uri((New-Object System.Uri("http://momoirocode.web.fc2.com/")), $savePath)
+                $link = New-Object System.Uri((New-Object System.Uri("http://momoirocode.web.fc2.com/")), $downloadFilePath)
                 Write-Debug "link: $link"
                 Invoke-WebRequest $link -OutFile $savePath
             }        
@@ -38,12 +41,13 @@ if (!$OnlyRecently) {
 $json[-2..-1] | % -parallel {
     $_.ImagesUrl | % {
         $DebugPreference = $using:DebugPreference
-        $savePath = Join-Path $using:saveDir $_
+        $downloadFilePath = Join-Path $using:saveDir $_
+        $savePath = Join-Path $using:saveDir "ja/"
         Write-Debug "savePath: $savePath"
         Write-Debug "sp-check: $savePath : $($using:spfile)"
         if ($savePath -ne $using:spfile){
             Write-Debug "new file(sp skip mode)"
-            $link = New-Object System.Uri((New-Object System.Uri("http://momoirocode.web.fc2.com/")), $savePath)
+            $link = New-Object System.Uri((New-Object System.Uri("http://momoirocode.web.fc2.com/")), $downloadFilePath)
             Write-Debug "link: $link"
             Invoke-WebRequest $link -OutFile $savePath
         }
