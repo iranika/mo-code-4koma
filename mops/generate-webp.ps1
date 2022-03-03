bd79ecc5543e6715657d1016908f8052a6b524a6
iranika <7370486+iranika@users.noreply.github.com>
2022-03-04 08:36:57 +0900

    fix: dont throw error.

--- mops/generate-webp.ps1
+++ mops/generate-webp.ps1
@@ -7,11 +7,12 @@ param (
 
 $base = pwd
 Set-Location ./4koma/ja
-
+Write-Debug "current dir: $(pwd)"
 if ($OnlyRecently){
+    Write-Debug "generate-webp is OnlyRecently mode;"
     (Get-ChildItem -Filter "*.jpg" | Sort-Object -Property LastWriteTime)[-5..-1] | % -Parallel {
         Write-Debug "webp fille: $($_.Name)"
-        ffmpeg -y -i $_ ("webp/$($_.Name)" -replace ".jpg",".webp")
+        ffmpeg -y -i $_.Name ("webp/$($_.Name)" -replace ".jpg",".webp") || "ffmpeg was done: $($_.Name)"
     }
 }else{
     $last5 = ((Get-ChildItem -Filter "*.jpg") | Sort-Object { $_.LastWriteTime })[-1..-5].Name
