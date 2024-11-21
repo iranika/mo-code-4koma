ec8000abd5fad6496f5392c223cf3b1011f8df01
iranika <7370486+iranika@users.noreply.github.com>
2024-11-21 09:29:18 +0000

    mops: find new update.

--- 4komaData.js
+++ 4komaData.js
@@ -3273,5 +3273,13 @@ pageData =
       "https://mo4koma.iranika.info/4koma/ja/sp.jpg"
     ],
     "Index": 359
+  },
+  {
+    "Title": "便利",
+    "ImagesUrl": [
+      "https://mo4koma.iranika.info/4koma/ja/360-1.jpg",
+      "https://mo4koma.iranika.info/4koma/ja/sp.jpg"
+    ],
+    "Index": 360
   }
 ]
