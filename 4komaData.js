b3f8c1e0c2ce6ccbaff83f8c484391c627168075
iranika <7370486+iranika@users.noreply.github.com>
2025-03-14 04:47:14 +0000

    mops: find new update.

--- 4komaData.js
+++ 4komaData.js
@@ -3337,7 +3337,10 @@ pageData =
     "Title": "ゴッブレスユー",
     "ImagesUrl": [
       "https://mo4koma.iranika.info/4koma/ja/366-1.jpg",
-      "https://mo4koma.iranika.info/4koma/ja/366-2.jpg",     "https://mo4koma.iranika.info/4koma/ja/sp.jpg"
+      "https://mo4koma.iranika.info/4koma/ja/366-2.jpg",
+      "https://mo4koma.iranika.info/4koma/ja/366-3.jpg",
+      "https://mo4koma.iranika.info/4koma/ja/366-4.jpg",
+      "https://mo4koma.iranika.info/4koma/ja/sp.jpg"
     ],
     "Index": 366
   }
