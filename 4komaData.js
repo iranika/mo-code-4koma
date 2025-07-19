d2f9e5a81e6c99d590388122db8764a5f370171a
iranika <7370486+iranika@users.noreply.github.com>
2025-07-19 03:43:02 +0000

    mops: find new update.

--- 4komaData.js
+++ 4komaData.js
@@ -3414,5 +3414,13 @@ pageData =
       "https://mo4koma.iranika.info/4koma/ja/sp.jpg"
     ],
     "Index": 373
+  },
+  {
+    "Title": "駄菓子",
+    "ImagesUrl": [
+      "https://mo4koma.iranika.info/4koma/ja/374-1.jpg",
+      "https://mo4koma.iranika.info/4koma/ja/sp.jpg"
+    ],
+    "Index": 374
   }
 ]
