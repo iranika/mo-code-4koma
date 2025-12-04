1be8d7ee3fcebe38c3215a0b39dd856e63d822f3
iranika <7370486+iranika@users.noreply.github.com>
2025-12-04 13:08:55 +0000

    mops: find new update.

--- 4komaData.js
+++ 4komaData.js
@@ -3493,5 +3493,13 @@ pageData =
       "https://mo4koma.iranika.info/4koma/ja/sp.jpg"
     ],
     "Index": 381
+  },
+  {
+    "Title": "日々是好日",
+    "ImagesUrl": [
+      "https://mo4koma.iranika.info/4koma/ja/382-1.jpg",
+      "https://mo4koma.iranika.info/4koma/ja/sp.jpg"
+    ],
+    "Index": 382
   }
 ]
