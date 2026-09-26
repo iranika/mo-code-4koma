45f6ca642cc9b71ab44cfcee7590f3670b2312a6
iranika <7370486+iranika@users.noreply.github.com>
2026-09-26 11:45:42 +0000

    mops: find new update.

--- 4komaData.js
+++ 4komaData.js
@@ -3685,6 +3685,7 @@ pageData =
     "Title": "あかとんぼ",
     "ImagesUrl": [
       "https://mo4koma.iranika.info/4koma/ja/402-1.jpg",
+      "https://mo4koma.iranika.info/4koma/ja/402-2.jpg",
       "https://mo4koma.iranika.info/4koma/ja/sp.jpg"
     ],
     "Index": 402
